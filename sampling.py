a6bdbfacc8d60b629a6cbb00a9d73bca0ad2b45f
Alex Wurts <ajwurts@gmail.com>
2018-11-12 17:51:27 -0500

    Started Fracctional Differentiation

--- sampling.py
+++ sampling.py
@@ -5,7 +5,11 @@ import pandas as pd
 
 # close = csv['Close']
 
-def cumsum(raw, h):
+def cumsum(raw, p):
+    """
+    takes in raw pricing and samples based on the price movement.
+    p is the percent threshold to activate
+    """
     pos = 0
     neg = 0
     diff = raw.diff()
@@ -14,13 +18,30 @@ def cumsum(raw, h):
         if i == 0: continue
         pos = max(0, pos + (diff[i] / raw[i]))
         neg = min(0, neg + (diff[i] / raw[i]))
-        if pos > h:
+        if pos > p:
             points.append(i)
             pos = 0
-        elif neg < -h:
+        elif neg < -p:
             points.append(i)
             neg = 0
 
     return points
+
+
+
+def fractionalDifferentiation(raw, t, d):
+
+    summation = 0
+    # raw = [t, ..., t-max]
+    raw = raw[t:]
+
+    for k in range(len(raw)):
+        product = -raw[k]
+        for i in range(len(data) - 1)):
+            product *= ((d - i) / (k - i))
+
+        summation += product  
+
+    return summmation      
 
 
