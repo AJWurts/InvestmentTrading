8c3de7dccb7e3d69e6626cda79829934de77c122
Alex Wurts <ajwurts@gmail.com>
2018-11-17 14:45:52 -0500

    Added side to getBins. Updated to newest SPY.csv

--- clusterfuck.py
+++ clusterfuck.py
@@ -34,50 +34,19 @@ def applyTripleBarrierLabeling(close, events, ptSl):
 
 
 
-def getDailyVol(close,span0=100):
+def getDailyVol(close,span0=50):
     # daily vol reindexed to close
-    df0=close.index.searchsorted(close.index-pd.Timedelta(days=1))
-    df0=df0[df0>0]   
-    df0=(pd.Series(close.index[df0-1], 
+    df0 = close.index.searchsorted(close.index-pd.Timedelta(days=1))
+    df0 = df0[df0 > 0]   
+    df0 = (pd.Series(close.index[df0 - 1], 
                    index=close.index[close.shape[0]-df0.shape[0]:]))   
     try:
-        df0=close.loc[df0.index]/close.loc[df0.values].values-1 # daily rets
+        df0 = close.loc[df0.index] / close.loc[df0.values].values - 1 # daily rets
     except Exception as e:
         print('error: {e}\nplease confirm no duplicate indices')
-    df0=df0.ewm(span=span0).std().rename('dailyVol')
+    df0 = df0.ewm(span=span0).std().rename('dailyVol')
     return df0
     
-# # create target series
-# ptsl = [1,1]
-# target=dailyVol
-# # select minRet
-# minRet = 0.01
-
-# # Run in single-threaded mode on Windows
-# import platform
-# if platform.system() == "Windows":
-#     cpus = 1
-# else:
-#     cpus = cpu_count() - 1
-    
-# events = getEvents(close,tEvents,ptsl,target,minRet,cpus,t1=t1)
-# def getEvents(close, tEvents, ptSl, trgt, minRet, numThreads, t1=False, side=None):
-#     #1) get target
-#     trgt=trgt.loc[tEvents]
-#     trgt=trgt[trgt>minRet] # minRet
-#     #2) get t1 (max holding period)
-#     if t1 is False:t1=pd.Series(pd.NaT, index=tEvents)
-#     #3) form events object, apply stop loss on t1
-#     if side is None:side_,ptSl_=pd.Series(1.,index=trgt.index), [ptSl[0],ptSl[0]]
-#     else: side_,ptSl_=side.loc[trgt.index],ptSl[:2]
-#     events=(pd.concat({'t1':t1,'trgt':trgt,'side':side_}, axis=1)
-#             .dropna(subset=['trgt']))
-#     df0=mpPandasObj(func=applyPtSlOnT1,pdObj=('molecule',events.index),
-#                     numThreads=numThreads,close=close,events=events,
-#                     ptSl=ptSl_)
-#     events['t1']=df0.dropna(how='all').min(axis=1) # pd.min ignores nan
-#     if side is None:events=events.drop('side',axis=1)
-#     return events
 
 def addVerticalBarrier(tEvents, close, numDays=1):
     t1=close.index.searchsorted(tEvents+pd.Timedelta(days=numDays))
@@ -92,20 +61,24 @@ def getBins(events, close):
     -events.index is event's starttime
     -events['t1'] is event's endtime
     -events['trgt'] is event's target
-    -events['side'] (optional) implies the algo's position side
+    -events['side'] (optional) implies the algo's position side. Adjusts so the bin is in terms of the algorithm
     Case 1: ('side' not in events): bin in (-1,1) <-label by price action
     Case 2: ('side' in events): bin in (0,1) <-label by pnl (meta-labeling)
     '''
     #1) prices aligned with events
-    events_=events.dropna(subset=['t1'])
-    px=events_.index.union(events_['t1'].values).drop_duplicates()
-    px=close.reindex(px,method='bfill')
+    events_ = events.dropna(subset=['t1'])
+    px = events_.index.union(events_['t1'].values).drop_duplicates()
+    px =  close.reindex(px,method='bfill')
     #2) create out object
-    out=pd.DataFrame(index=events_.index)
-    out['ret']=px.loc[events_['t1'].values].values/px.loc[events_.index]-1
-    if 'side' in events_:out['ret']*=events_['side'] # meta-labeling
-    out['bin']=np.sign(out['ret'])
-    if 'side' in events_:out.loc[out['ret']<=0,'bin']=0 # meta-labeling
+    out = pd.DataFrame(index=events_.index)
+    out['ret'] = px.loc[events_['t1'].values].values / px.loc[events_.index] - 1
+
+    if 'side' in events_:
+        out['ret'] *= events_['side'] # meta-labeling
+    out['bin'] = np.sign(out['ret'])
+
+    if 'side' in events_:
+        out.loc[out['ret'] <= 0,'bin'] = 0  # meta-labeling
     return out
 
 if __name__ == "__main__":
@@ -121,7 +94,7 @@ if __name__ == "__main__":
 
     events = cumsum(dbars, 0.01)
     
-    t1 = addVerticalBarrier(events, data['Close'], numDays=10)
+    t1 = addVerticalBarrier(events, data['Close'], numDays=3)
     trgt = dailyVol
     side_ = pd.Series(1.,index=trgt.index)
 
@@ -129,7 +102,9 @@ if __name__ == "__main__":
     
     out = applyTripleBarrierLabeling(data['Close'], events, [1,1] )
 
+    out['side'] = side_
     bins = getBins(out, data['Close'])
+    print(bins)
 
 
     buying = bins[bins.bin == 1.0]
