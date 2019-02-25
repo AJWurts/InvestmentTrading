ee62748dc6d34feb7629df138b49fa5069f0118e
Alexander Wurts <ajwurts@gmail.com>
2019-02-25 00:48:14 -0500

    Laptop Changes

--- backtesting/backtester.py
+++ backtesting/backtester.py
@@ -70,12 +70,15 @@ def backTester(algorithm, close_prices, pt=1.01, sl=0.99, exp=5):
     cash = STARTING_MONEY
     stock_owned = 0
     trades = Trades()
+    historical_positions = []
     positions = []
     for i, p in tqdm(enumerate(close_prices), total=len(close_prices)):
         choice, amt = algorithm(p, cash, stock_owned)
         keepPositions = []
         for pos in positions:
             if pos.sl >= p or pos.pt <= p or pos.exp <= i or i == len(close_prices) - 1:
+                pos.setSellTime(i, p)
+                historical_positions.append(pos)
                 cash += (pos.qtn * p - COMMISSION)
                 trades.addTrade(Trade(i, pos.qtn * p, 'sell'))
                 stock_owned -= pos.qtn
@@ -86,7 +89,7 @@ def backTester(algorithm, close_prices, pt=1.01, sl=0.99, exp=5):
         if choice == 'buy':
             if cash < amt * p:
                 amt = int(cash / p)
-            positions.append(Position(i, amt, p * sl, p * pt, i + exp))
+            positions.append(Position(i, amt, p * sl, p * pt, i + exp, p))
             trades.addTrade(Trade(i, amt, 'buy'))
             stock_owned += amt
             cash -= (amt * p + COMMISSION)
@@ -95,7 +98,7 @@ def backTester(algorithm, close_prices, pt=1.01, sl=0.99, exp=5):
             print(str(i), cash + stock_owned * p)
         value_history.append(cash + stock_owned * p)
     cash += stock_owned * close_prices[-1]
-    return cash, value_history, trades
+    return cash, value_history, trades, historical_positions
 
 
 def start_backtest():
@@ -108,12 +111,14 @@ def start_backtest():
     # names = ['algo 1', 'algo 2', 'algo 3']
     _, axs = plt.subplots(len(algorithms), 1, sharex=True)
     for i, algo in enumerate(algorithms):
-        result, history, trades = backTester(algo, data['Close'].values, sl=0.99, pt=1.01, exp=3)
+        result, history, trades, positions = backTester(algo, data['Close'].values, sl=0.97, pt=1.03, exp=3)
+        with open(names[i] + '.txt', 'w') as output:
+            output.write(str(positions))
         roi = "{:.2f}%".format((result - STARTING_MONEY) / STARTING_MONEY * 100)
         axs[i].set_title(names[i] + 'return: ' + roi + ' trades: ' + str(len(trades)))
         axs[i].plot([i for i in range(len(history))], history,  color='blue', linewidth=2)
-        axs[i].scatter(trades.getTradeX('sell'), [history[t.time] for t in trades.tradeLog if t.type == 'sell'], color='red')
-        axs[i].scatter(trades.getTradeX('buy'), [history[t.time] - 10 for t in trades.tradeLog if t.type == 'buy'] , color='green')
+        axs[i].scatter(trades.getTradeX('sell'), [history[t.time] for t in trades.tradeLog if t.type == 'sell'], color='red', alpha=0.5)
+        axs[i].scatter(trades.getTradeX('buy'), [history[t.time] - 10 for t in trades.tradeLog if t.type == 'buy'] , color='green', alpha=0.5)
         reset()
         print(names[i] + " Result: $" + str(result))
         print("ROI: {:.2f}%".format(
--- backtesting/trade.py
+++ backtesting/trade.py
@@ -1,13 +1,22 @@
 
 class Position:
-    def __init__(self, time, qtn, sl=None, pt=None, exp=None):
+    def __init__(self, time, qtn, sl=None, pt=None, exp=None, buy_price=None):
         self.start_time = time
         self.sell_time = None
+        self.sell_price = None
+        self.buy_price = buy_price
         self.qtn = qtn
         self.sl = sl
         self.pt = pt
         self.exp = exp
-    
-    def setSellTime(sell_time):
-        self.sell_time = sell_time):
-    
+
+    def setSellTime(self, sell_time, sell_price):
+        self.sell_time = sell_time
+        self.sell_price = sell_price
+
+    def __repr__(self):
+        return "Return: {2}, Pct return: {0}, Length: {1}\n".format(
+            (self.sell_price - self.buy_price) / self.buy_price,
+            self.sell_time - self.start_time,
+            (self.sell_price * self.qtn) - (self.buy_price * self.qtn)
+        )
